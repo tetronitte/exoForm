55a2c7724b745dc77ed8080fe9188bceee57b5a8
tetronitte <68910823+tetronitte@users.noreply.github.com>
2021-02-04 20:12:35 +0100

    debug

--- controller/form_controller.php
+++ controller/form_controller.php
@@ -1,5 +1,11 @@
 <?php
 
+session_start();
+unset($_SESSION['form']);
+unset($_SESSION['error']);
+unset($_SESSION['adressError']);
+unset($_SESSION['formSend']);
+
 $countries = array("Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe");
 $degreeArray = ['sans','BAC+2','BAC+3','BAC+5','Doctorat'];
 $errors = array();
@@ -10,13 +16,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(isset($_POST['lastname'])) {
         $lastname = validData($_POST['lastname']);
         if(!regexString($lastname)) array_push($errors,1);
-        $form[] = $lastname;
+        $form['lastname'] = $lastname;
     }
     //Vérification prénom
     if(isset($_POST['firstname'])) {
         $firstname = validData($_POST['firstname']);
         if(!regexString($firstname)) array_push($errors,2);
-        $form[] = $firstname;
+        $form['firstname'] = $firstname;
     }
     //Vérification date de naissance
     if(isset($_POST['birthday'])) {
@@ -26,6 +32,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             $birthdayError = "Date de naissance non renseignée";
         }
         $birthday = validData($_POST['birthday']);
+        $validBirthday = $birthday;
         $birthday = transformDate($birthday);
         $birthdayYear = substr($birthday,6);
         $limitYear = date('Y',mktime(0,0,0,date('m'),date('d'),date('y')-15));
@@ -37,19 +44,19 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             array_push($errors,3);
             $birthdayError = "Erreur sur la date de naissance";
         }
-        $form[] = $birthday;
+        $form['birthday'] = $validBirthday;
     }
      //Vérification pays de naissance
     if(isset($_POST['country'])) {
         $country = validData($_POST['country']);
         if(!regexString($country) || !in_array($country,$countries)) array_push($errors,4);
-        $form[] = $country;
+        $form['country'] = $country;
     }
     //Vérification nationalité
     if(isset($_POST['nationality'])) {
         $nationality = validData($_POST['nationality']);
         if(!regexString($nationality)) array_push($errors,5);
-        $form[] = $nationality;
+        $form['nationality'] = $nationality;
     }
     //Vérification Adresse
         $adressError = "";
@@ -82,39 +89,40 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 array_push($errors,6);
                 $adressError = "Cette adresse n'existe pas";
             }
-            $form[] = $city;
-            $form[] = $postal;
-            $form[] = $street;
+            $form['city'] = $city;
+            $form['postal'] = $postal;
+            $form['street'] = $street;
         }
     //Vérification email
     if(isset($_POST['email'])) {
         $email = validData($_POST['email']);
         if(!filter_var($email,FILTER_VALIDATE_EMAIL)) array_push($errors,7);
-        $form[] = $email;
+        $form['email'] = $email;
     }
     //Vérification téléphone
     if(isset($_POST['tel'])) {
         $tel = validData($_POST['tel']);
         if(!preg_match('/^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/',$tel)) array_push($errors,8);
-        $form[] = $tel;
+        $form['tel'] = $tel;
     }
     //Vérification diplôme
     if(isset($_POST['degree'])) {
         $degree = validData($_POST['degree']);
         if(!in_array($degree,$degreeArray)) array_push($errors,9);
-        $form[] = $degree;
+        var_dump($degree);
+        $form['degree'] = $degree;
     }
     //Vérification numéro pole emploi
     if(isset($_POST['poleEmploiNumber'])) {
         $poleEmploiNumber = validData($_POST['poleEmploiNumber']);
         if(!preg_match('/^([0-9]{6})([A-Z]{1})$/',$poleEmploiNumber)) array_push($errors,10);
-        $form[] = $poleEmploiNumber;
+        $form['poleEmploiNumber'] = $poleEmploiNumber;
     }
     //Vérification nombre de badge
     if(isset($_POST['badgeCount'])) {
         $badgeCount = validData($_POST['badgeCount']);
         if(!preg_match('/^[0-9]{1,2}$/',$badgeCount)) array_push($errors,11);
-        $form[] = $badgeCount;
+        $form['badgeCount'] = $badgeCount;
     }
     //Vérification codecademy
     if(isset($_POST['codecademy'])) {
@@ -131,32 +139,30 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             $domain = $hostParts[1].'.'.$hostParts[0];
         }
         if($domain != 'codecademy.com') array_push($errors,15);
-        $form[] = $codecademy;
+        $form['codecademy'] = $codecademy;
     }
     //Vérification héros
     if(isset($_POST['heroes'])) {
         $heroes = validData($_POST['heroes']);
         if(!regexTextarea($heroes)) array_push($errors,12);
-        else $heroes = nl2br($heroes);
-        $form[] = $heroes;
+        $form['heroes'] = $heroes;
     }
     //Vérification hack
     if(isset($_POST['hack'])) {
         $hack = validData($_POST['hack']);
         if(!regexTextarea($hack)) array_push($errors,13);
-        else $hack = nl2br($hack);
-        $form[] = $hack;
+        $form['hack'] = $hack;
     }
     //Vérification de l'expérience
     if(isset($_POST['experience'])) {
         $experience = validData($_POST['experience']);
         if(!regexTextarea($experience)) array_push($errors,14);
-        else $experience = nl2br($experience);
-        $form[] = $experience;
+        $form['experience'] = $experience;
     }
-    session_start();
     $_SESSION['error'] = $errors;
     $_SESSION['form'] = $form;
+    $_SESSION['adressError'] = $adressError;
+    $_SESSION['formSend'] = '<div id="1"></div>';
     header('Location: http://localhost/exoForm/');
     exit();
 }
@@ -194,7 +200,7 @@ function regexString($string) {
 }
 
 function regexTextarea($textarea) {
-    return preg_match('/^([0-9a-zA-ZàáâäæçéèêëîïôœùûüÿÀÂÄÆÇÉÈÊËÎÏÖÔŒÙÛÜŸ \-\'\n\.\!\"\,\-]+)$/',$textarea);
+    return preg_match('/^([0-9a-zA-ZàáâäæçéèêëîïôœùûüÿÀÂÄÆÇÉÈÊËÎÏÖÔŒÙÛÜŸ \-\'\r\.\!\"\,\-]+)$/',$textarea);
 }
 
 function regexStreet($street) {
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
     session_start();
-
 ?>
 
 <!doctype html>
@@ -10,7 +9,8 @@
         <link rel="stylesheet" href="http://localhost/exoForm/assets/css/form.css">
     </head>
     <body>
-        <form action="index.php" method="POST">
+        <?php if(isset($_SESSION['formSend'])) echo $_SESSION['formSend']; ?>
+        <form action="http://localhost/exoForm/controller/form_controller.php" method="POST">
             <div class="panneau-deroulant-1" id="panneau1">
                 <div class="row-title" id="row_title1">
                     <span class="title-panneau">Informations identitaires</span>
@@ -20,27 +20,27 @@
                 <div class="row-form">
                     <label for="lastname">Nom : </label>
                     <input type="text" name="lastname" placeholder="O'connor/De Beauclair" value="<?php if(isset($_SESSION['form'])) echo $_SESSION['form']['lastname']; ?>"/>
-                    <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur le nom" alt="Erreur sur le nom" width="30" height='30' <?php if(isset($_SESSION['error']) && !in_array(1,$_SESSION['error']) || empty($_SESSION['error'])) echo 'hidden'; ?>>
+                    <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur le nom" alt="Erreur sur le nom" width="30" height='30' <?php if((isset($_SESSION['error']) && !in_array(1,$_SESSION['error'])) || empty($_SESSION['error'])) echo 'hidden'; ?>>
                 </div>
                 <div class="row-form">
                     <label for="firstname">Prénom : </label>
                     <input type="text" name="firstname" placeholder="Jean-Sébastien" value="<?php if(isset($_SESSION['form'])) echo $_SESSION['form']['firstname']; ?>"/>
-                    <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur le prénom" alt="Erreur sur le prénom" width="30" height='30' <?php if(isset($_SESSION['error']) && !in_array(2,$_SESSION['error']) || empty($_SESSION['error'])) echo 'hidden'; ?>>
+                    <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur le prénom" alt="Erreur sur le prénom" width="30" height='30' <?php if((isset($_SESSION['error']) && !in_array(2,$_SESSION['error'])) || empty($_SESSION['error'])) echo 'hidden'; ?>>
                 </div>
                 <div class="row-form">
                     <label for="birthday">Date de naissance : </label>
                     <input type="date" name="birthday" id="birthday" value="<?php if(isset($_SESSION['form'])) echo $_SESSION['form']['birthday']; ?>"/>
-                    <img src="http://localhost/exoForm/assets/img/error.png" title="<?php if($_SERVER["REQUEST_METHOD"] == "POST") echo $birthdayError; ?>" alt="Erreur sur la date de naissance" width="30" height='30' <?php if(isset($_SESSION['error']) && !in_array(3,$_SESSION['error']) || empty($_SESSION['error'])) echo 'hidden'; ?>>
+                    <img src="http://localhost/exoForm/assets/img/error.png" title="" alt="Erreur sur la date de naissance" width="30" height='30' <?php if((isset($_SESSION['error']) && !in_array(3,$_SESSION['error'])) || empty($_SESSION['error'])) echo 'hidden'; ?>>
                 </div>
                 <div class="row-form">
                     <label for="country">Pays de naissance : </label>
                     <input type="text" name="country" placeholder="France" value="<?php if(isset($_SESSION['form'])) echo $_SESSION['form']['country']; ?>"/>
-                    <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur le pays" alt="Erreur sur le pays de naissance" width="30" height='30' <?php if(isset($_SESSION['error']) && !in_array(4,$_SESSION['error']) || empty($_SESSION['error'])) echo 'hidden'; ?>>
+                    <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur le pays" alt="Erreur sur le pays de naissance" width="30" height='30' <?php if((isset($_SESSION['error']) && !in_array(4,$_SESSION['error'])) || empty($_SESSION['error'])) echo 'hidden'; ?>>
                 </div>
                 <div class="row-form">
                     <label for="nationality">Nationalité : </label>
                     <input type="text" name="nationality" placeholder="française" value="<?php if(isset($_SESSION['form'])) echo $_SESSION['form']['nationality']; ?>"/>
-                    <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur la nationalité" alt="Erreur sur la nationalité" width="30px" height='30px' <?php if(isset($_SESSION['error']) && !in_array(5,$_SESSION['error']) || empty($_SESSION['error'])) echo 'hidden'; ?>>
+                    <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur la nationalité" alt="Erreur sur la nationalité" width="30px" height='30px' <?php if((isset($_SESSION['error']) && !in_array(5,$_SESSION['error'])) || empty($_SESSION['error'])) echo 'hidden'; ?>>
                 </div>
             </div>
             <div class="panneau-deroulant-2" id="panneau2">
@@ -54,28 +54,28 @@
                     <input type="text" name="street" placeholder="N° rue" value="<?php if(isset($_SESSION['form'])) echo $_SESSION['form']['street']; ?>"/>
                     <input type="text" name="city" placeholder="Ville" value="<?php if(isset($_SESSION['form'])) echo $_SESSION['form']['city']; ?>"/>
                     <input type="text" name="postal" placeholder="Code postal" value="<?php if(isset($_SESSION['form'])) echo $_SESSION['form']['postal']; ?>"/>
-                    <img src="http://localhost/exoForm/assets/img/error.png" title="<?php if($_SERVER["REQUEST_METHOD"] == "POST") echo $adressError; ?>" width="30px" height='30px' <?php if(isset($_SESSION['error']) && !in_array(6,$_SESSION['error']) || empty($_SESSION['error'])) echo 'hidden'; ?>>
+                    <img src="http://localhost/exoForm/assets/img/error.png" title="<?php if(isset($_SESSION['adressError'])) echo $_SESSION['adressError'] ?>" width="30px" height='30px' <?php if((isset($_SESSION['error']) && !in_array(6,$_SESSION['error'])) || empty($_SESSION['error'])) echo 'hidden'; ?>>
                 </div>
                 <div class="row-form">
                     <label for="email">Email : </label>
                     <input type="text" name="email" placeholder="léa.vanghzertahlt@google.com" value="<?php if(isset($_SESSION['form'])) echo $_SESSION['form']['email']; ?>"/>
-                    <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur l'email" alt="Erreur sur l'email" width="30px" height='30px' <?php if(isset($_SESSION['error']) && !in_array(7,$_SESSION['error']) || empty($_SESSION['error'])) echo 'hidden'; ?>>
+                    <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur l'email" alt="Erreur sur l'email" width="30px" height='30px' <?php if((isset($_SESSION['error']) && !in_array(7,$_SESSION['error'])) || empty($_SESSION['error'])) echo 'hidden'; ?>>
                 </div>
                 <div class="row-form">
                     <label for="tel">Téléphone : </label>
                     <input type="text" name="tel" placeholder="+336 45 37 82 73/0645362739" value="<?php if(isset($_SESSION['form'])) echo $_SESSION['form']['tel']; ?>"/>
-                    <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur le téléphone" alt="Erreur sur le téléphone" width="30" height='30' <?php if(isset($_SESSION['error']) && !in_array(8,$_SESSION['error']) || empty($_SESSION['error'])) echo 'hidden'; ?>>
+                    <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur le téléphone" alt="Erreur sur le téléphone" width="30" height='30' <?php if((isset($_SESSION['error']) && !in_array(8,$_SESSION['error'])) || empty($_SESSION['error'])) echo 'hidden'; ?>>
                 </div>
                 <div class="row-form">
                     <label for="degree">Diplôme : </label>
                     <select name="degree">
-                        <option value="sans" <?php if(isset($_SESSION['form']) && $_SESSION['form']['degree'] == 'sans') echo 'selected' ?>>sans</option>
-                        <option value="BAC+2" <?php if(isset($_SESSION['form']) && $_SESSION['form']['degree'] == 'BAC+2') echo 'selected' ?>>BAC+2</option>
-                        <option value="BAC+3" <?php if(isset($_SESSION['form']) && $_SESSION['form']['degree'] == 'BAC+3') echo 'selected' ?>>BAC+3</option>
-                        <option value="BAC+5" <?php if(isset($_SESSION['form']) && $_SESSION['form']['degree'] == 'BAC+5') echo 'selected' ?>>BAC+5</option>
-                        <option value="Doctorat" <?php if(isset($_SESSION['form']) && $_SESSION['form']['degree'] == 'Doctorat') echo 'selected' ?>>Doctorat</option>
+                        <option value="sans" <?php if(isset($_SESSION['form']) && $_SESSION['form']['degree'] == "sans") echo 'selected'; ?>>sans</option>
+                        <option value="BAC+2" <?php if(isset($_SESSION['form']) && $_SESSION['form']['degree'] == "BAC+2") echo 'selected'; ?>>BAC+2</option>
+                        <option value="BAC+3" <?php if(isset($_SESSION['form']) && $_SESSION['form']['degree'] == "BAC+3") echo 'selected'; ?>>BAC+3</option>
+                        <option value="BAC+5" <?php if(isset($_SESSION['form']) && $_SESSION['form']['degree'] == "BAC+5") echo 'selected'; ?>>BAC+5</option>
+                        <option value="Doctorat" <?php if(isset($_SESSION['form']) && $_SESSION['form']['degree'] == "Doctorat") echo 'selected'; ?>>Doctorat</option>
                     </select>
-                    <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur le diplôme" alt="Erreur sur le diplôme" width="30" height='30' <?php if(isset($_SESSION['error']) && !in_array(9,$_SESSION['error']) || empty($_SESSION['error'])) echo 'hidden'; ?>>
+                    <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur le diplôme" alt="Erreur sur le diplôme" width="30" height='30' <?php if((isset($_SESSION['error']) && !in_array(9,$_SESSION['error'])) || empty($_SESSION['error'])) echo 'hidden'; ?>>
                 </div>
             </div>
             <div class="panneau-deroulant-3" id="panneau3">
@@ -87,17 +87,17 @@
                 <div class="row-form">
                     <label for="poleEmploiNumber">Numéro pôle emploi : </label>
                     <input type="text" name="poleEmploiNumber" placeholder="647382N" value="<?php if(isset($_SESSION['form'])) echo $_SESSION['form']['poleEmploiNumber']; ?>"/>
-                    <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur le numéro pole emploi" alt="Erreur sur le numéro pole emploi" width="30" height='30' <?php if(isset($_SESSION['error']) && !in_array(10,$_SESSION['error']) || empty($_SESSION['error'])) echo 'hidden'; ?>>
+                    <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur le numéro pole emploi" alt="Erreur sur le numéro pole emploi" width="30" height='30' <?php if((isset($_SESSION['error']) && !in_array(10,$_SESSION['error'])) || empty($_SESSION['error'])) echo 'hidden'; ?>>
                 </div>
                 <div class="row-form">
                     <label for="badgeCount">Nombre de badge : </label>
                     <input type="text" name="badgeCount" placeholder="0 à 99" value="<?php if(isset($_SESSION['form'])) echo $_SESSION['form']['badgeCount']; ?>"/>
-                    <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur le nombre de badge" alt="Erreur sur le nombre de badge" width="30" height='30' <?php if(isset($_SESSION['error']) && !in_array(11,$_SESSION['error']) || empty($_SESSION['error'])) echo 'hidden'; ?>>
+                    <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur le nombre de badge" alt="Erreur sur le nombre de badge" width="30" height='30' <?php if((isset($_SESSION['error']) && !in_array(11,$_SESSION['error'])) || empty($_SESSION['error'])) echo 'hidden'; ?>>
                 </div>
                 <div class="row-form">
                     <label for="codecademy">Lien codecademy : </label>
                     <input type="text" name="codecademy" placeholder="http://www.codecademy.com" value="<?php if(isset($_SESSION['form'])) echo $_SESSION['form']['codecademy']; ?>"/>
-                    <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur le lien codecademy" alt="Erreur sur le lien codecademy" width="30" height='30' <?php if(isset($_SESSION['error']) && !in_array(15,$_SESSION['error']) || empty($_SESSION['error'])) echo 'hidden'; ?>>
+                    <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur le lien codecademy" alt="Erreur sur le lien codecademy" width="30" height='30' <?php if((isset($_SESSION['error']) && !in_array(15,$_SESSION['error'])) || empty($_SESSION['error'])) echo 'hidden'; ?>>
                 </div>
             </div>
             <div class="panneau-deroulant-4" id="panneau4">
@@ -110,21 +110,21 @@
                     <label for="heroes">Si vous étiez un super héros/une super héroïne, qui seriez-vous et pourquoi ? </label><br/>
                     <div class="area-img">                
                         <textarea rows="2" cols="60" wrap="hard" name="heroes" placeholder="Vous pouvez utiliser plusieurs lignes ainsi que certains caractères spéciaux commes les virgules"><?php if(isset($_SESSION['form'])) echo $_SESSION['form']['heroes']; ?></textarea>
-                        <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur quel héros vous voulez être" alt="Erreur sur quel héros vous voulez être" width="30" height='30' <?php if(isset($_SESSION['error']) && !in_array(12,$_SESSION['error']) || empty($_SESSION['error'])) echo 'hidden'; ?>>
+                        <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur quel héros vous voulez être" alt="Erreur sur quel héros vous voulez être" width="30" height='30' <?php if((isset($_SESSION['error']) && !in_array(12,$_SESSION['error'])) || empty($_SESSION['error'])) echo 'hidden'; ?>>
                     </div>
                 </div>
                 <div class="row-form area">
                     <label for="hack">Racontez-nous un de vos "hacks" (pas forcément technique ou informatique) </label><br/>
                     <div class="area-img">
                         <textarea rows="2" cols="60" wrap="hard" name="hack" placeholder="Vous pouvez utiliser plusieurs lignes ainsi que certains caractères spéciaux commes les virgules"><?php if(isset($_SESSION['form'])) echo $_SESSION['form']['hack']; ?></textarea>
-                        <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur vos hacks" alt="Erreur sur vos hacks" width="30" height='30' <?php if(isset($_SESSION['error']) && !in_array(13,$_SESSION['error']) || empty($_SESSION['error'])) echo 'hidden'; ?>>
+                        <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur vos hacks" alt="Erreur sur vos hacks" width="30" height='30' <?php if((isset($_SESSION['error']) && !in_array(13,$_SESSION['error'])) || empty($_SESSION['error'])) echo 'hidden'; ?>>
                     </div>
                 </div>
                 <div class="row-form area">
                     <label for="experience">Avez vous déjà eu une expérience avec la programmation<br/> et/ou l'informatique avant de remplir ce formulaire ? </label><br/>
                     <div class="area-img">
                         <textarea rows="2" cols="60" wrap="hard" name="experience" placeholder="Vous pouvez utiliser plusieurs lignes ainsi que certains caractères spéciaux commes les virgules"><?php if(isset($_SESSION['form'])) echo $_SESSION['form']['experience']; ?></textarea>
-                        <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur vos expériences" alt="Erreur sur vos expériences" width="30" height='30' <?php if(isset($_SESSION['error']) && !in_array(14,$_SESSION['error']) || empty($_SESSION['error'])) echo 'hidden'; ?>>
+                        <img src="http://localhost/exoForm/assets/img/error.png" title="Erreur sur vos expériences" alt="Erreur sur vos expériences" width="30" height='30' <?php if((isset($_SESSION['error']) && !in_array(14,$_SESSION['error'])) || empty($_SESSION['error'])) echo 'hidden'; ?>>
                     </div>
                 </div>
             </div>
